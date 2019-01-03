566a9d3443d750123424464a1e5832df42b7c0c8
bmartin <buddym@tracesecurity.com>
2019-01-03 13:58:31 -0600

    Revert "Revert "Finished report tab""
    This reverts commit 75dd40db2a2f7b98a7e487d434737ef64efec6ad.

--- csv test-6.28.18/Preview.Designer.cs
+++ csv test-6.28.18/Preview.Designer.cs
@@ -40,11 +40,13 @@
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.Size = new System.Drawing.Size(718, 359);
             this.dataGridView.TabIndex = 2;
+            this.dataGridView.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_RowHeaderMouseDoubleClick);
             // 
             // Preview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(760, 397);
             this.Controls.Add(this.dataGridView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
